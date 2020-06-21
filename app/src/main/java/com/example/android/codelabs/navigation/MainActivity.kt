2a92102f9e99dc9ffc9ccc326df18a6dfb2a9d07
Drew Sommer <drew@sommerengineering.com>
2020-06-20 17:47:42 -0700

    feat: Adapt notification UI based on device screen size

--- app/src/main/java/com/example/android/codelabs/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/MainActivity.kt
@@ -24,12 +24,11 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.google.android.material.bottomnavigation.BottomNavigationMenu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
@@ -55,22 +54,21 @@ class MainActivity : AppCompatActivity() {
         // Set up Action Bar
         val navController = host.navController
 
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        // alternate method
+        // appBarConfiguration = AppBarConfiguration(navController.graph)
 
-        // TODO STEP 9.5 - Create an AppBarConfiguration with the correct top-level destinations
-        // You should also remove the old appBarConfiguration setup above
-//        val drawerLayout : DrawerLayout? = findViewById(R.id.drawer_layout)
-//        appBarConfiguration = AppBarConfiguration(
-//                setOf(R.id.home_dest, R.id.deeplink_dest),
-//                drawerLayout)
-        // TODO END STEP 9.5
+        // define and pair "top-level" (global access) destinations with drawer items via matching ids
+        // top-level dest removes toolbar up-arrow and replaces it with nothing, or hamburger if drawer exists
+        // graph start destination always top-level
+        val drawer : DrawerLayout? = findViewById(R.id.drawer_layout)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.home_dest, R.id.deeplink_dest), drawer)
 
+        // helpers configure activity level navigation
         setupActionBar(navController, appBarConfiguration)
-
         setupNavigationMenu(navController)
-
         setupBottomNavMenu(navController)
 
+        // callback for any navigation changes
         navController.addOnDestinationChangedListener { _, destination, _ ->
             val dest: String = try {
                 resources.getResourceName(destination.id)
@@ -78,35 +76,29 @@ class MainActivity : AppCompatActivity() {
                 Integer.toString(destination.id)
             }
 
-            Toast.makeText(this@MainActivity, "Navigated to $dest", Toast.LENGTH_SHORT).show()
+            // Toast.makeText(this@MainActivity, "Navigated to $dest", Toast.LENGTH_SHORT).show()
             Log.d("NavigationActivity", "Navigated to $dest")
         }
     }
 
     private fun setupBottomNavMenu(navController: NavController) {
 
-        // connect the controller to XML navigation
+        // connect controller to bottom navigation XML
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
         bottomNav?.setupWithNavController(navController)
     }
 
     private fun setupNavigationMenu(navController: NavController) {
-        // TODO STEP 9.4 - Use NavigationUI to set up a Navigation View
-//        // In split screen mode, you can drag this view out from the left
-//        // This does NOT modify the actionbar
-//        val sideNavView = findViewById<NavigationView>(R.id.nav_view)
-//        sideNavView?.setupWithNavController(navController)
-        // TODO END STEP 9.4
+
+        // connect controller to side navigation XML
+        val sideNav = findViewById<NavigationView>(R.id.nav_view)
+        sideNav?.setupWithNavController(navController)
     }
 
-    private fun setupActionBar(navController: NavController,
-                               appBarConfig : AppBarConfiguration) {
-        // TODO STEP 9.6 - Have NavigationUI handle what your ActionBar displays
-//        // This allows NavigationUI to decide what label to show in the action bar
-//        // By using appBarConfig, it will also determine whether to
-//        // show the up arrow or navigation menu icon
-//        setupActionBarWithNavController(navController, appBarConfig)
-        // TODO END STEP 9.6
+    private fun setupActionBar(navController: NavController, appBarConfig : AppBarConfiguration) {
+
+        // connect controller to action bar (toolbar) which includes automatic up-arrow behavior
+        setupActionBarWithNavController(navController, appBarConfig)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -121,21 +113,19 @@ class MainActivity : AppCompatActivity() {
         return retValue
     }
 
-    // settings icon is click
+    // settings icon is clicked
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        // search through nav_menu graph for this item id
+        // search through nav graph for this item id
         return item.onNavDestinationSelected(findNavController(R.id.my_nav_host_fragment))
 
                 // this menu item is not meant to navigate
                 || super.onOptionsItemSelected(item)
     }
 
-    // TODO STEP 9.7 - Have NavigationUI handle up behavior in the ActionBar
-//    override fun onSupportNavigateUp(): Boolean {
-//        // Allows NavigationUI to support proper up navigation or the navigation layout
-//        // navigation menu, depending on the situation
-//        return findNavController(R.id.my_nav_host_fragment).navigateUp(appBarConfiguration)
-//    }
-    // TODO END STEP 9.7
+    override fun onSupportNavigateUp(): Boolean {
+
+        // action bar configuration defines up behavior (nothing, arrow, menu)
+        return findNavController(R.id.my_nav_host_fragment).navigateUp(appBarConfiguration)
+    }
 }
